811699e92ce82ab98e782eb8b08fd7427c35fd5e
Jayaleshwari <jayaleshwarin@syncfusion.com>
2018-11-30 14:41:36 +0530

    Sample committed

---
+++ SfListViewSample/SfListViewSample/SfListViewSample/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Syncfusion.DataSource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace SfListViewSample
+{
+    [Preserve(AllMembers = true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+      
+    }
+}
